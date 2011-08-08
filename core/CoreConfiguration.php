ee64f9ff1047bc5c1077c55bd979210a7d125ea1
Fabian M <mail.fabianm@gmail.com>
2011-08-08 22:07:36 +0200

    FaabBB Version 3.006 ALPHA

--- core/CoreConfiguration.php
+++ core/CoreConfiguration.php
@@ -2,7 +2,7 @@
 /**
  * Contains <code>core</code> configuration, like contant variables.
  * When defining a constant, remember they are constantly the same, so you 
- * 	may not the the value at runtime. 
+ * 	may not change the the value at runtime. 
  */
 
 define('FaabBB_VERSION', '3.006 ALPHA');

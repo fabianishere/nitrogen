13347b641012d04e981819ebfdedd549344a2bfd
Fabian M <mail.fabianm@gmail.com>
2011-09-25 17:28:09 +0200

    Woah, git added some code to my code. Fixed it now.

--- data/config/run.ini.php
+++ data/config/run.ini.php
@@ -1,7 +1,4 @@
 <?php exit(); ?>
 ; Run configuration file for the core.
-<<<<<<< HEAD
 faabbb = org/faabtech/examples/HelloWorld
-=======
-faabbb = org/faabtech/faabbb/FaabBB
->>>>>>> 9a6d2c3467051785defd155c7f1ae76204dd03f4
+
